5206928e57bf5e289536546b2fe60de56c623be3
RegalEagles2869 <theregaleagles2869@gmail.com>
2023-09-29 15:56:17 -0400

    made path thing I guess
    possibly

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -133,6 +133,16 @@ public static UsbCamera camera;
   /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
   @Override
   public void autonomousInit() {
+	/*
+	// This will load the file "Example Path.path" and generate it with a max velocity of 4 m/s and a max acceleration of 3 m/s^2
+	PathPlannerTrajectory examplePath = PathPlanner.loadPath("Example Path", new PathConstrains(4, 3));
+
+	// This trajectory can then be passed to a path follower such as a PPSwerveControllerCommand
+	// Or the path can be sampled at a given point in time for custom path following
+
+	// Sample the state of the path at 1.2 seconds
+	PathPlannerState exampleState = (PathPlannerState) examplePath.sample(1.2);
+	 */
     // Shuffleboard.selectTab("Auto");
 	PivotSubsystem.getInstance().brake();
 	WristSubsystem.getInstance().brake();
